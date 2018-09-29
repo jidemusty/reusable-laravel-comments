54305baf79c5197b568964fd31e08ee1a702f1cb
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-09-28 23:30:23 -0500

    comments and replies

---
+++ database/migrations/2018_09_29_041623_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->text('body');
+            $table->integer('parent_id')->unsigned()->index()->nullable();
+            $table->morphs('commentable'); // shortcut for creating commentable_id, commentable_type
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
