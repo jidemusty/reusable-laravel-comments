579acd95c6cac4420c143fd09d48e9839df2b342
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-09-28 23:49:00 -0500

    commentable controller

---
+++ app/Http/Controllers/Courses/CourseCommentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Courses;
+
+use App\Course;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+
+class CourseCommentController extends Controller
+{
+    public function index(Course $course)
+    {
+        return CommentResource::collection(
+            $course->comments()->with(['children', 'user'])->get()
+        );
+    }
+}
