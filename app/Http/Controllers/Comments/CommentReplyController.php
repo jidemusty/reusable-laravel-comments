838ed7fe07517d4025dbd0485694654a3e5de448
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-10-06 21:02:25 -0500

    replying to a comment

---
+++ app/Http/Controllers/Comments/CommentReplyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Comments;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CommentResource;
+
+class CommentReplyController extends Controller
+{
+    public function store(Comment $comment, Request $request)
+    {
+        $this->authorize('reply', $comment);
+
+        $this.validate($request, [
+            'body' => 'required | max:5000'
+        ]);
+
+        $reply = $comment->children()->make([
+            'body' => $request->body
+        ]);
+
+        $reply->commentable()->associate($comment->commentable);
+
+        $request->user()->comments()->save($reply);
+
+        return new CommentResource($reply);
+    }
+}
